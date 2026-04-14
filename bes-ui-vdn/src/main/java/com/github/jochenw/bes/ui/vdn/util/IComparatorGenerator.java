70964b5e45ea7863fd2afae68fe981614b88738e
Jochen Wiedmann <wiedmann.j@zdf.de>
2026-04-14 11:22:29 +0200

    Rebooting the project (again)

--- bes-ui-vdn/src/main/java/com/github/jochenw/bes/ui/vdn/util/IComparatorGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.github.jochenw.bes.ui.vdn.util;
-
-import java.util.Comparator;
-
-public interface IComparatorGenerator {
-	public <T> Comparator<T> getComparator(Class<T> pType, boolean pCaseSensitive);
-}
